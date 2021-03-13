72447c6b3ea3cc87175ba295d33c16ade9e1dd1c
polesmih <polesmih@yandex.ru>
2021-03-13 18:43:48 +0300

    Homework level 2 lesson 2

---
+++ src/hw_lesson_2/MyArrayDataException1.java
@@ -0,0 +1,7 @@
+package hw_lesson_2;
+
+public class MyArrayDataException1 extends RuntimeException {
+    public MyArrayDataException1(String s) {
+        super(s);
+    }
+}
---
+++ src/hw_lesson_2/MyArraySizeException1.java
@@ -0,0 +1,7 @@
+package hw_lesson_2;
+
+public class MyArraySizeException1 extends RuntimeException {
+    public MyArraySizeException1(String s) {
+        super(s);
+    }
+}
